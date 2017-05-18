657189638ff2de2e9dfe702cc8d0fdbfe10d5935
Ricardo Alvim <ricardoalvim@alunos.utfpr.edu.br>
2017-05-17 22:54:21 -0300

    Script de banco de dados atualizado! Postgres compat

--- script/0. Database.sql
+++ script/0. Database.sql
@@ -8,72 +8,202 @@
 
 CREATE DATABASE r4c ENCODING = 'UTF8';
 --\c r4c;
+----------------
+-- Questionary -
+----------------
+CREATE TABLE IF NOT EXISTS Questionary (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(45) NULL,
+  start DATE NULL,
+  ends DATE NULL,
+  briefing VARCHAR(45) NULL,
+  slug VARCHAR(45) NULL,
+  active BOOL NULL);
 
-CREATE TABLE Project(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(120),
-    briefing VARCHAR(254),
-    dateStarts DATE,
-    dateEnds DATE,
-    active BOOLEAN
+-------------
+-- Project --
+-------------
+CREATE TABLE IF NOT EXISTS Project (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(45) NULL,
+  briefing VARCHAR(45) NULL,
+  starts DATE NULL,
+  ends DATE NULL,
+  active BOOL NULL,
+  questionary INT NOT NULL,
+  CONSTRAINT Project_has_Questionary
+    FOREIGN KEY (questionary)
+    REFERENCES Questionary(id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-CREATE TABLE Questionary(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(200),
-    dateStart DATE,
-    dateEnds DATE,
-    briefing VARCHAR(120),
-    slug VARCHAR(100),
-    active BOOLEAN
+
+-- -----------------------------------------------------
+-- Table Question
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Question (
+  id SERIAL PRIMARY KEY,
+  question VARCHAR(45) NULL,
+  questionary INT NOT NULL,
+  CONSTRAINT Question_below_questionary
+    FOREIGN KEY (questionary)
+    REFERENCES Questionary (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-CREATE TABLE Question(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    question VARCHAR(200),
-    questionary INTEGER,
-    CONSTRAINT question_belows_questionary 
-    FOREIGN KEY (questionary) REFERENCES Questionary(id)
+-- -----------------------------------------------------
+-- Table TypeProfile
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS TypeProfile (
+  id INT NOT NULL,
+  name VARCHAR(45) NULL,
+  active BOOL NOT NULL,
+  description VARCHAR(45) NULL,
+  level INT NULL,
+  PRIMARY KEY (id)
 );
 
--- alterar
-CREATE TABLE Anwser(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    anw VARCHAR(160),
-    question INTEGER,
-    nextQuestion INTEGER,
-    score INTEGER,
-    CONSTRAINT anwser_belows_question
-    FOREIGN KEY (question) REFERENCES Question(id),
 
-    CONSTRAINT nextQuestion
-    FOREIGN KEY (nextQuestion) REFERENCES Question(id)
+-- -----------------------------------------------------
+-- Table Profile
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Profile (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(45) NOT NULL,
+  mail VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NULL,
+  typeProfile INT NOT NULL,
+  city VARCHAR(45) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  sex VARCHAR(1) NOT NULL,
+  studies VARCHAR(45) NOT NULL,
+  position VARCHAR(45) NOT NULL,
+  CONSTRAINT Profile_has_type
+    FOREIGN KEY (typeProfile)
+    REFERENCES TypeProfile (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-CREATE TABLE TypeProfile(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(120),
-    active BOOLEAN,
-    description VARCHAR(160),
-    level INTEGER
+
+CREATE TABLE IF NOT EXISTS Anwser (
+  id SERIAL PRIMARY KEY,
+  awnser VARCHAR(45) NULL,
+  score INT NULL,
+  question SERIAL NOT NULL,
+  profile SERIAL NOT NULL,
+  CONSTRAINT Awnser_has_Question
+    FOREIGN KEY (question)
+    REFERENCES Question (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT Awnser_has_Profile
+    FOREIGN KEY (profile)
+    REFERENCES Profile (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION  
 );
 
-CREATE TABLE Profile(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(120),
-    email VARCHAR(60),
-    password VARCHAR(60),
-    username VARCHAR(60)
+-- -----------------------------------------------------
+-- Table QuestionaryProfile
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS QuestionaryProfile (
+  level INT NOT NULL,
+  profile SERIAL NOT NULL,
+  questionary SERIAL NOT NULL,
+  CONSTRAINT ToProfile
+    FOREIGN KEY (profile)
+    REFERENCES Profile (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT ToQuestionary
+    FOREIGN KEY (questionary)
+    REFERENCES Questionary (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-CREATE TABLE QuestionaryProfile(
-    profile INTEGER,
-    questionary INTEGER,
-    level INTEGER,
 
-    CONSTRAINT profileAccess
-    FOREIGN KEY (profile) REFERENCES Profile(id),
+-- -----------------------------------------------------
+-- Table Statues
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Statues (
+  id SERIAL PRIMARY KEY,
+  state VARCHAR(45) NOT NULL
+);
 
-    CONSTRAINT questionaryAllowed
-    FOREIGN KEY (questionary) REFERENCES Questionary(id)
+
+-- -----------------------------------------------------
+-- Table Task
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Task (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(45) NOT NULL,
+  dueDate DATE NULL,
+  statues SERIAL NOT NULL,
+  awnser SERIAL NOT NULL,
+
+  CONSTRAINT taskStatues
+    FOREIGN KEY (statues)
+    REFERENCES Statues (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    
+  CONSTRAINT taskswnser
+    FOREIGN KEY (awnser)
+    REFERENCES Anwser (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table Task_has_Project
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Task_has_Project (
+  task SERIAL NOT NULL,
+  project SERIAL NOT NULL,
+
+  CONSTRAINT task
+    FOREIGN KEY (task)
+    REFERENCES Task (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+
+  CONSTRAINT project
+    FOREIGN KEY (project)
+    REFERENCES Project (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table TagPattern
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS TagPattern (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(45) NOT NULL
+);
+
+
+-- -----------------------------------------------------
+-- Table Anwser_has_TagPattern
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Anwser_has_TagPattern (
+  anwser INT NOT NULL,
+  tagPattern INT NOT NULL,
+
+  CONSTRAINT awnser
+    FOREIGN KEY (anwser)
+    REFERENCES Anwser (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT tagPattern
+    FOREIGN KEY (tagPattern)
+    REFERENCES TagPattern (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
